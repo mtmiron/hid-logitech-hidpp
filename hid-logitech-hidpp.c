ddca6becbed33b83f673dbfe411a58e447897b10
Murray Miron <mmiron@srllc.org>
2022-05-10 11:08:45 -0400

    Only report low-res events as a quick fix for quirkiness

--- hid-logitech-hidpp.c
+++ hid-logitech-hidpp.c
@@ -472,20 +472,13 @@ static void hidpp_scroll_counter_handle_scroll(struct input_dev *input_dev,
 					       struct hidpp_scroll_counter *counter,
 					       int hi_res_value)
 {
-	int low_res_value, remainder, direction;
-	unsigned long long now, previous;
+	int remainder, direction;
 
-	//hi_res_value = hi_res_value * 120/counter->wheel_multiplier;
-	//hi_res_value = hi_res_value;
-	//input_report_rel(input_dev, REL_WHEEL_HI_RES, hi_res_value);
 	remainder = counter->remainder;
 	direction = hi_res_value > 0 ? 1 : -1;
 
-	now = sched_clock();
-	previous = counter->last_time;
-	counter->last_time = now;
 	remainder += hi_res_value;
-	if (abs(remainder) / 20 > 0) {
+	if (abs(remainder) / 30 > 0) {
 		input_report_rel(input_dev, REL_WHEEL, direction);
 		counter->remainder = 0;
 	} else {
@@ -495,54 +488,7 @@ static void hidpp_scroll_counter_handle_scroll(struct input_dev *input_dev,
 			counter->remainder += remainder;
 	}
 	counter->direction = direction;
-		//if (counter->direction != direction)
-//			counter->remainder = 0;
-//		else
-//			counter->remainder = remainder;
-//	}
 	return;
-	if (now - previous < 500000000)
-		return;
-	direction = hi_res_value < 0 ? -1 : 1;
-	input_report_rel(input_dev, REL_WHEEL, direction);//hi_res_value / 100 + direction);
-	return;
-	if (abs(hi_res_value / 4) > 0)
-		input_report_rel(input_dev, REL_WHEEL, hi_res_value / 4);
-	else
-		remainder = hi_res_value;
-	//printk(KERN_ERR "at time %lld: %d", now - previous, hi_res_value);
-	return;
-	/*
-	 * Reset the remainder after a period of inactivity or when the
-	 * direction changes. This prevents the REL_WHEEL emulation point
-	 * from sliding for devices that don't always provide the same
-	 * number of movements per detent.
-	 */
-	//if (now - previous > 1000000000)// || direction != counter->direction)
-	if (now - previous > 1000000000 || ((now - previous > 1000) && direction != counter->direction))
-		remainder = 0;
-
-	counter->direction = direction;
-	remainder += hi_res_value;
-
-	/* Some wheels will rest 7/8ths of a detent from the previous detent
-	 * after slow movement, so we want the threshold for low-res events to
-	 * be in the middle between two detents (e.g. after 4/8ths) as
-	 * opposed to on the detents themselves (8/8ths).
-	 */
-	if (abs(remainder) >= 60) {
-		/* Add (or subtract) 1 because we want to trigger when the wheel
-		 * is half-way to the next detent (i.e. scroll 1 detent after a
-		 * 1/2 detent movement, 2 detents after a 1 1/2 detent movement,
-		 * etc.).
-		 */
-		low_res_value = remainder / 120;
-		if (low_res_value == 0)
-			low_res_value = (hi_res_value > 0 ? 1 : -1);
-		//input_report_rel(input_dev, REL_WHEEL, low_res_value);
-		remainder -= low_res_value * 120;
-	}
-	counter->remainder = remainder;
 }
 
 /* -------------------------------------------------------------------------- */
