9711702176ea404fc32199243b292115a4396085
Murray Miron <mmiron@srllc.org>
2022-05-15 01:58:53 -0400

    Re-enable reporting of hi-res wheel movements

--- hid-logitech-hidpp.c
+++ hid-logitech-hidpp.c
@@ -472,23 +472,48 @@ static void hidpp_scroll_counter_handle_scroll(struct input_dev *input_dev,
 					       struct hidpp_scroll_counter *counter,
 					       int hi_res_value)
 {
-	int remainder, direction;
+	int low_res_value, remainder, direction;
+	unsigned long long now, previous;
+
+	hi_res_value = hi_res_value * 120/counter->wheel_multiplier;
+	input_report_rel(input_dev, REL_WHEEL_HI_RES, hi_res_value);
 
 	remainder = counter->remainder;
 	direction = hi_res_value > 0 ? 1 : -1;
 
-	remainder += hi_res_value;
-	if (abs(remainder) / 30 > 0) {
-		input_report_rel(input_dev, REL_WHEEL, direction);
-		counter->remainder = 0;
-	} else {
-		if (counter->direction != direction)
-			counter->remainder = hi_res_value;
-		else
-			counter->remainder += remainder;
-	}
+	now = sched_clock();
+	previous = counter->last_time;
+	counter->last_time = now;
+	/*
+	 * Reset the remainder after a period of inactivity or when the
+	 * direction changes. This prevents the REL_WHEEL emulation point
+	 * from sliding for devices that don't always provide the same
+	 * number of movements per detent.
+	 */
+	if (now - previous > 1000000000)// || direction != counter->direction)
+		remainder = 0;
+
 	counter->direction = direction;
-	return;
+	remainder += hi_res_value;
+
+	/* Some wheels will rest 7/8ths of a detent from the previous detent
+	 * after slow movement, so we want the threshold for low-res events to
+	 * be in the middle between two detents (e.g. after 4/8ths) as
+	 * opposed to on the detents themselves (8/8ths).
+	 */
+	if (abs(remainder) >= 60) {
+		/* Add (or subtract) 1 because we want to trigger when the wheel
+		 * is half-way to the next detent (i.e. scroll 1 detent after a
+		 * 1/2 detent movement, 2 detents after a 1 1/2 detent movement,
+		 * etc.).
+		 */
+		low_res_value = remainder / 120;
+		if (low_res_value == 0)
+			low_res_value = (hi_res_value > 0 ? 1 : -1);
+		input_report_rel(input_dev, REL_WHEEL, low_res_value);
+		remainder -= low_res_value * 120;
+	}
+	counter->remainder = remainder;
 }
 
 /* -------------------------------------------------------------------------- */
